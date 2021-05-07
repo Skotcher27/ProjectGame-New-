3cc4b2abb774b49651183b73557e0ab2d859e96a
Skotcher27 <jekc1990@mail.ru>
2021-05-07 13:12:15 +0500

    Пересоздал файл

--- Form1.cs
+++ Form1.cs
@@ -12,9 +12,20 @@ namespace ProjectGame
 {
     public partial class Form1 : Form
     {
+        public Bitmap HandlerTexture = Resource1.Handler,
+                      TargetTexture = Resource1.Target;
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics g = e.Graphics;
+
+            g.DrawImage(HandlerTexture, new Rectangle(0, 0, 100, 100));
+            g.DrawImage(TargetTexture, new Rectangle(0, 0, 100, 100));
+
         }
     }
 }
