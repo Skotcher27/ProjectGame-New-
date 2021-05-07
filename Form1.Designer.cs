ad281063dc6e7e7dad5fef6b516c30b54659f9f8
Skotcher27 <jekc1990@mail.ru>
2021-05-07 13:47:40 +0500

    3 неделя
    Создал двигающийся объект и курсор

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -29,14 +29,28 @@ namespace ProjectGame
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
             this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 25;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // timer2
+            // 
+            this.timer2.Enabled = true;
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackgroundImage = global::ProjectGame.Resource1.Background;
-            this.ClientSize = new System.Drawing.Size(941, 582);
+            this.ClientSize = new System.Drawing.Size(1089, 613);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
@@ -45,6 +59,9 @@ namespace ProjectGame
         }
 
         #endregion
+
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Timer timer2;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -14,17 +14,53 @@ namespace ProjectGame
     {
         public Bitmap HandlerTexture = Resource1.Handler,
                       TargetTexture = Resource1.Target;
+        private Point _targetPosition = new Point(300, 300);
+        private Point _direction = Point.Empty;
         public Form1()
         {
             InitializeComponent();
+            SetStyle(ControlStyles.OptimizedDoubleBuffer | ControlStyles.AllPaintingInWmPaint | ControlStyles.UserPaint, true);
+            UpdateStyles();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            Refresh();
+        }
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            Random r = new Random();
+
+            timer2.Interval = r.Next(25, 1000);
+            _direction.X = r.Next(-1, 2);
+            _direction.Y = r.Next(-1, 2);
         }
 
         private void Form1_Paint(object sender, PaintEventArgs e)
         {
             Graphics g = e.Graphics;
+            var LocalPosition = this.PointToClient(Cursor.Position);
 
-            g.DrawImage(HandlerTexture, new Rectangle(0, 0, 100, 100));
-            g.DrawImage(TargetTexture, new Rectangle(0, 0, 100, 100));
+            _targetPosition.X += _direction.X * 10;
+            _targetPosition.Y += _direction.Y * 10;
+
+            if(_targetPosition.X < 0 ||  _targetPosition.X > 500 )
+            {
+                _direction.X = 200;
+               
+            }
+            if (_targetPosition.Y < 0 || _targetPosition.Y > 500)
+            {
+               
+                _direction.Y = 200;
+            }
+
+            var handlerRect = new Rectangle(LocalPosition.X - 50, LocalPosition.Y - 50, 100, 100);
+            var targetRect = new Rectangle(_targetPosition.X - 50, _targetPosition.Y - 50, 100, 100);
+
+            g.DrawImage(HandlerTexture, handlerRect);
+            g.DrawImage(TargetTexture, targetRect);
 
         }
     }
